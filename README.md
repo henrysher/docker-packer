1f94f3c4e7d9d8be789ee1f0b3028c8bc803f136
root <root@localhost.localdomain>
2015-06-03 04:12:23 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # docker-packer
 docker image to build the latest packer: https://github.com/mitchellh/packer
+
+```
+# mkdir -p ${PWD}/dist
+# docker run --privileged -v ${PWD}/dist:/dist  henrysher/build-packer
+```
+```
+# ls ${PWD}/dist
+packer-latest-20150603063457.zip
+```
---
+++ build_packer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -x
+go get -u github.com/mitchellh/gox
+mkdir -p /root/go/src/github.com/mitchellh/
+git clone https://github.com/mitchellh/packer.git /root/go/src/github.com/mitchellh/packer
+cd /root/go/src/github.com/mitchellh/packer; make updatedeps; make; make dev
+
+mkdir -p /dist
+WORKSPACE=/dist
+cd /root/go/bin; zip -9 ${WORKSPACE}/packer-latest-`date +%Y%m%d%H%M%S`.zip packer*
